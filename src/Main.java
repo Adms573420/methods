4d85ae8afdd8186f8dd0557a06bb3363f3b1519f
Garcia <adem.okur5734@gmail.com>
2022-10-14 22:34:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(topla(3, 6));
+
+        mesajVer();
+        System.out.println(carpim(5,7));
+    }
+
+
+
+
+    public static int topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public static void mesajVer() {
+
+        System.out.println("Bugun hava cok guzel");
+
+    }
+
+    public static int carpim(int sayi1, int sayi2) {
+
+        return sayi1*sayi2;
+
+    }
+
+
+}
